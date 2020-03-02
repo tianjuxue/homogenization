1d74d4beb74fedd20426738d09425472112db507
Tianju Xue <tianjuxue@nat-tvwna-inside-visitornet101-10-24-111-111.princeton.org>
2020-03-02 14:43:19 -0500

    fix inspection plot

--- src/collector/exp.py
+++ src/collector/exp.py
@@ -7,8 +7,74 @@ from .. import arguments
 import fenics as fa
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
 
+def compute_shear(generator):
+    generator.void_shape = np.array([-0.2, 0.2])
+    x_vals = np.linspace(-0.7, 0.7, 21)
+    energy = []
+    for x_val in x_vals:
+        generator.args.F_list_fixed = [[0, x_val], [0., -0.125]]
+        generator.def_grad = np.array([0, x_val, 0, -0.125])
+        generator._anealing_solver_fluctuation(False)
+        energy_density = generator.energy_density
+        energy.append(energy_density[-1])
+        print(generator.energy_density)
+        print(generator.probe_all)
+
+    energy = np.asarray(energy)
+    np.savez('plots/new_data/numpy/inspection/shear.npz',
+             H12=x_vals,
+             shear_energy=energy
+             )
+
+
+def compute_normal(generator):
+    generator.void_shape = np.array([-0., 0.])
+    x_vals = np.linspace(-0.08, 0.08, 11)
+    energy = []
+    for x_val in x_vals:
+        generator.args.F_list_fixed = [[x_val, 0], [0., -0.125]]
+        generator.def_grad = np.array([x_val, 0,  0, -0.125])
+        generator._anealing_solver_fluctuation(False)
+        energy_density = generator.energy_density
+        energy.append(energy_density[-1])
+        print(generator.energy_density)
+        print(generator.probe_all)
+
+    energy = np.asarray(energy)
+    np.savez('plots/new_data/numpy/inspection/normal.npz',
+             H11=x_vals,
+             normal_energy=energy
+             )
+
+
+def plot_shear():
+    data = np.load('plots/new_data/numpy/inspection/shear.npz')
+    H12 = data['H12']
+    energy = data['shear_energy']
+    fig = plt.figure()
+    plt.xticks(np.arange(min(H12), max(H12) + 1,
+                         step=(max(H12) - min(H12)) / 4))
+    plt.tick_params(labelsize=14)
+    plt.plot(H12, energy, '-*', label='energy')
+    plt.show()
+    fig.savefig("plots/new_data/images/shear.pdf", bbox_inches='tight')
+
+
+def plot_normal():
+    data = np.load('plots/new_data/numpy/inspection/normal.npz')
+    H11 = data['H11']
+    energy = data['normal_energy']
+    fig = plt.figure()
+    plt.xticks(np.arange(min(H11), max(H11) + 1,
+                         step=(max(H11) - min(H11)) / 4))
+    plt.tick_params(labelsize=14)
+    plt.plot(H11, np.flip(energy), '-*', label='energy')
+    plt.show()
+    fig.savefig("plots/new_data/images/normal.pdf", bbox_inches='tight')
+
+
+if __name__ == '__main__':
 
     args = arguments.args
     PATH = args.data_path_integrated_regular
@@ -20,78 +86,19 @@ if __name__ == '__main__':
     generator.args.n_cells = 2
     generator.args.metamaterial_mesh_size = 15
     generator.args.fluctuation = True
-    generator.void_shape = np.array([-0., 0.]) 
-    generator.anneal_factors = np.linspace(0, 1, 21)
+    generator.anneal_factors = np.linspace(0, 1, 11)
 
+    plot_normal()
+    plot_shear()
 
-    x_vals = np.linspace(-0.7, 0.7, 3)
+    # compute_normal(generator)
+    # compute_shear(generator)
 
-    pr = []
-    energy = []
-
-    for x_val in x_vals:
-        generator.args.F_list_fixed = [[0, x_val], [0., -0.125]]
-        generator.def_grad = np.array([0, x_val, 0, -0.125])        
-        generator._anealing_solver_fluctuation(False)
-        energy_density = generator.energy_density
-        energy.append(energy_density[-1])
-        pr.append(x_val/0.125)
-        print(generator.energy_density)
-        print(generator.probe_all)        
-
-
-    energy = np.asarray(energy)
-
-    fig = plt.figure()
-    plt.tick_params(labelsize=16)
-    plt.xlabel("shear strain")
-    plt.ylabel("strain energy density")
-    plt.plot(x_vals, energy, '-*', label='energy')          
-    # plt.legend(loc='lower right')
-    plt.show()
-    fig.savefig("foo.pdf", bbox_inches='tight')
-
-    exit()
-
-
-    try:
-        generator._anealing_solver_fluctuation(False)
-        print(generator.energy_density)
-        print(generator.probe_all)
-    except Exception as e:
-        print(e)
-        print(generator.energy_density)
-        print(generator.probe_all)
-    
-    exit()
-
-    generator.args.fluctuation = False
-    generator.args.F_list_fixed = np.array([[0.1, 0], [0, -0.125]])
-
-
-    x_vals = np.linspace(0, -0.05, 10)
-    energy = []
-
-    for x_val in x_vals:
-
-        def_grad = np.array([x_val, 0, 0, -0.125])
-        void_shape = np.array([0.1, -0.05]) 
-
-        generator.def_grad = def_grad
-        generator.void_shape = void_shape
-
-        generator.anneal_factors = np.linspace(0, 1, 3)
-
-        energy_density = generator._anealing_solver_fluctuation(False)
-        energy.append(energy_density[-1])
-
-        print(energy_density[-1])
-
-    plt.figure()
-    energy = np.asarray(energy)
-    plt.plot(x_vals, energy, '-*', label='energy')          
-    plt.legend(loc='upper right')
-    plt.show()
-
-
-    # print(energy_density, force)
+    # try:
+    #     generator._anealing_solver_fluctuation(False)
+    #     print(generator.energy_density)
+    #     print(generator.probe_all)
+    # except Exception as e:
+    #     print(e)
+    #     print(generator.energy_density)
+    #     print(generator.probe_all)
